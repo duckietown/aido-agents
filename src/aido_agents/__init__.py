ac37fc7c5fcb44e887714ce2f3a602c1022d9dfc
Andrea Censi <acensi@ethz.ch>
2020-11-16 00:23:35 +0100

    fix

--- src/aido_agents/__init__.py
+++ src/aido_agents/__init__.py
@@ -3,7 +3,7 @@ __version__ = "6.0.13"
 from zuper_commons.logs import ZLogger
 
 logger = ZLogger(__name__)
-logger.debug(f"aido-agents version {__version__}")
+logger.debug(f"aido-agents version {__version__} path {__file__}")
 
 from .utils_images import *
 from .utils_leds import *
